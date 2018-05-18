33e3dd24dff2f447ec589c85644d9088d6076747
hankliu5 <yliu224@ncsu.edu>
2018-05-18 10:18:59 -0400

    Event handlers in Angular.

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-simple-form',
   template: `<div>
   <input #myInput type="text">
-  <button (click)="onClick(myInput.value)">Click me!</button>
+  <button (click)="onClick($event, myInput.value)">Click me!</button>
   </div>`,
   styles: []
 })
@@ -12,7 +12,8 @@ export class SimpleFormComponent implements OnInit {
 
   constructor() { }
 
-  onClick(value) {
+  onClick(event, value) {
+    console.log(event);
     console.log(value);
   }
 
