e2542355a7ac05c050924b6f4dcf52e330d6aa74
hankliu5 <yliu224@ncsu.edu>
2018-05-18 10:29:40 -0400

    create a mail module and inject to app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MailService } from './mail.service';
 
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
-  template: `<div><app-simple-form></app-simple-form></div>`
+  template: `<div><app-simple-form>
+  </app-simple-form>
+    {{mail.message}}
+  </div>`
 })
 export class AppComponent {
   title = 'Fundamentals';
+
+  constructor(private mail: MailService) {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SimpleFormComponent } from './simple-form/simple-form.component';
+import { MailService } from './mail.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { SimpleFormComponent } from './simple-form/simple-form.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [MailService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
